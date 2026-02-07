77955df463a14e92d6ff81316efe04046bb22691
Marcos <marcosalessandrocunhacarvalho@gmail.com>
2026-02-07 04:46:13 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
 import Title from '../../components/Title'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Rinkashi17.png" />
-    <Title fontSize={16}>Marcos</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={16}>Marcos</Title>
+      <Paragrafo fontSize={16} tipo="secundario">
+        Rinkashi17
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
